e278ff0ed0ee00eb793fbdac059e77b308eb25ae
jakubmironczuk123 <jakub.mironczuk@tlen.pl>
2017-10-02 15:27:10 +0200

    zadanie 1

---
+++ projektnr2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+ {
+     int roz;
+     cout<<"podaj rozmiar trojkata"<<endl;
+     cin>>roz;
+     for(int i = 0; i<roz; i++)
+     {
+         for(int j=(roz-i);j>=0;j--)
+         {
+             cout<<" ";
+
+         }
+         for(int k =1;k<=2*i+1;k++)
+         {
+             cout<<"o";
+         }
+         cout<<endl;
+     }
+ }
